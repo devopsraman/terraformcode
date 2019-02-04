17b08bb8aea2010f1df9c5cd766150e8e074c4bf
Ram Thalla <devopsraman@gmail.com>
2019-02-04 14:07:34 +0000

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,16 +1,5 @@
 
-terraform {
-  backend "azurerm" {
-    storage_account_name  = "tstate09762"
-    container_name        = "tstate"
-    key                   = "terraform.tfstate"
-  }
-}
 
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state-demo"
-  location = "eastus"
-}
 
 
 # Configure the Microsoft Azure Provider
