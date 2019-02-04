dadb0b4b42d66a452f1671c7331cc1bda4cc8c34
Ram Thalla <devopsraman@gmail.com>
2019-02-04 13:57:24 +0000

    Update test.tf

--- test.tf
+++ test.tf
@@ -27,3 +27,10 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
         environment = "Terraform Demo"
     }
 }
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.1.0/24"
+}
