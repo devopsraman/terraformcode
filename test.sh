4fc7e0ba6421daf50e61d3b58ba914630c662fc8
Ram Thalla <devopsraman@gmail.com>
2019-04-24 11:31:10 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@ cp -R terraformcode/*.sh infrav1.0
 cp -R terraformcode/*.tf infrav1.0
 cp -R terraformcode/D* infrav1.0
 cp -R terraformcode/P* infrav1.0
-fpm -s dir -t rpm -n infra -v 1.1 infrav1.0
+fpm -s dir -t rpm -n infra -v 1.1 --description"testingcode" infrav1.0
