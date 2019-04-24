cd82fa1e935446c2aac5ff2d49ff945069197a1a
Ram Thalla <devopsraman@gmail.com>
2019-04-24 11:27:26 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+mkdir infrav1.0
+cp -R terraformcode/*.sh infrav1.0
+cp -R terraformcode/*.tf infrav1.0
+cp -R terraformcode/D* infrav1.0
+cp -R terraformcode/P* infrav1.0
+fpm -s dir -t rpm -n infra -v 1.1 infrav1.0
