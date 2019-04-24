d4bc1711ce8dde75ec665cd7079236472889bf43
Ram Thalla <devopsraman@gmail.com>
2019-04-24 12:27:25 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cp -R *.sh infrav1.0
 cp -R *.tf infrav1.0
 cp -R D* infrav1.0
 
-fpm -s dir -t rpm -n infra -v 1.1 --description"testingcode" infrav1.0
+fpm -s dir -t rpm -n infra -v 1.1 --description testingcode infrav1.0
